8ed13604140265b4663aa13f07efc1b1a0d04d6d
Blake <bag3cn@virginia.edu>
2016-02-14 14:56:14 -0500

    additional services

--- SockExchange/views.py
+++ SockExchange/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 from .models import Sock
 import json 
 
 # Create your views here.
 from django.http import HttpResponse
-
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
@@ -13,20 +12,18 @@ def id(request, sock_id):
 	sock = get_object_or_404(Sock, pk=sock_id)
 	return HttpResponse(json.dumps(sock.as_json()), content_type="application/json")
 
-	#response = "You're looking at the results of sock %s."
-
-
 def material(request, material):
-	response = "You're looking at the results of sock %s."
-	return HttpResponse(response % material)
+	sock = get_list_or_404(Sock, material=material)
+	results = [ob.as_json() for ob in sock]
+	return HttpResponse(json.dumps(results), content_type="application/json")
 
 def color(request, color):
-	response = "You're looking at the results of sock %s."
-	return HttpResponse(response % color)	
+	sock = get_list_or_404(Sock, color=color)
+	results = [ob.as_json() for ob in sock]
+	return HttpResponse(json.dumps(results), content_type="application/json")
 
 def theme(request, theme):
-	response = "You're looking at the results of sock %s."
-	return HttpResponse(response % theme)	
+	sock = get_list_or_404(Sock, theme=theme)
+	results = [ob.as_json() for ob in sock]
+	return HttpResponse(json.dumps(results), content_type="application/json")
 
-
-	#return HttpResponse(json.dumps(result.as_json()), content_type="application/json")
